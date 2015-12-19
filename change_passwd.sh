14c25aa4db13b2bf6a645e7563f521f862c043de
Hassane Sangare <sangare@intechinfo.fr>
2015-12-19 11:03:54 +0100

    modification password et correction script suppression compte mail

---
+++ change_passwd.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#script qui change le mot de passe de l'utilisateur
+
+nom=$1
+pass=$2
+
+/bin/sed -i "/$nom[[:space:]]/d" /etc/courier/userdb
+/usr/sbin/makeuserdb
+
+/usr/sbin/userdb $nom set uid=1006 gid=1006 home=/var/openmail/$nom mail=/var/openmail/$nom
+/bin/echo $pass | /usr/sbin/userdbpw -md5 | /usr/sbin/userdb "$nom" set systempw
+/usr/sbin/makeuserdb
--- del_mail_account.sh
+++ del_mail_account.sh
@@ -5,23 +5,20 @@ user=$1
 
 if [ -z $user ];then
         echo "ERREUR veuillez entrer un nom"
-        #exit
 else
         verif=`/bin/grep $user /etc/postfix/vmailbox`
         if [ -z "$verif" ];then
                 echo "Ce compte n'existe pas donc ne peut etre supprime"
-                #exit
         else
                 #suppression de la ligne dans vmailbox
                 /bin/sed -i "/$user@/d" /etc/postfix/vmailbox
                 /usr/sbin/postmap /etc/postfix/vmailbox
-                #/etc/init.d/postfix restart
 
                 #suppression du repertoire du compte
                 `/bin/rm -r /var/mail/$user`
 
                 #suppression dans le fichier userdb
-                /bin/sed -i "/$user/d" /etc/courier/userdb
+                /bin/sed -i "/$user[[:space:]]/d" /etc/courier/userdb
                 /usr/sbin/makeuserdb
         fi
 fi
