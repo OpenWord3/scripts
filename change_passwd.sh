f92f2cbcac00545dadc60beebf112c450aa78892
Hassane Sangare <sangare@intechinfo.fr>
2015-12-20 21:22:28 +0100

    fehvbdehbvdhuv

--- change_passwd.sh
+++ change_passwd.sh
@@ -8,6 +8,7 @@ pass=$2
 /bin/sed -i "/$nom[[:space:]]/d" /etc/courier/userdb
 /usr/sbin/makeuserdb
 
-/usr/sbin/userdb $nom set uid=1010 gid=1010 home=/var/openmail/$nom mail=/var/openmail/$nom
+/usr/sbin/userdb $nom set home=/var/mail/$nom mail=/var/mail/$nom uid=1010 gid=1010
 /bin/echo $pass | /usr/sbin/userdbpw -md5 | /usr/sbin/userdb "$nom" set systempw
+
 /usr/sbin/makeuserdb
